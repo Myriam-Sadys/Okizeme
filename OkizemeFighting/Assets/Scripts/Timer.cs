b55c9126f2148b86894202611e5082ecb6cd6d7d
Myriam <myriam.sadys@epitech.eu>
2019-08-24 15:37:22 +0000

    Revert "Merge branch 'develop' of https://gitlab.com/okizeme/okizeme-client into develop"
    This reverts commit 397ab9374cc2508383fe17617c6127816cb548c5

--- OkizemeFighting/Assets/Scripts/Timer.cs
+++ OkizemeFighting/Assets/Scripts/Timer.cs
@@ -8,49 +8,50 @@ namespace SA
 {
     public class Timer : MonoBehaviour
     {
-        public int StartValue = 99;
-        private int Value;
-        private float Delta = 0.0f;
+        public float StartValue = 99.0f;
+        private float Value;
         public Text text;
-        public Player Player1;
-        public Player Player2;
-        public Element attackElement;
+        //public PlayerMoving player1;
+        //public PlayerMoving player2;
+        //public Element attackElement;
+
 
         void Start()
         {
             Debug.Log("Fight");
             Value = StartValue;
         }
+
         void Update()
         {
-            if (Player1.IsAlive() && Player2.IsAlive())
-            {
-                Delta += Time.fixedDeltaTime;
-                Value = (int)((float)StartValue - (Delta % 60.0f));
-                text.text = Value.ToString();
-
-                //DEBUG
-                if (Input.GetKeyDown("p"))
+            //if (player1 && player2)
+            //{
+                if (/*player1.IsAlive() && player2.IsAlive() &&*/ Value > 0.1)
                 {
-                    Value -= 10;
-                }
-            }
-            else
-            {
-                if (Player1.IsAlive())
-                {
-                    StaticClass.Blocker.CardInstanceToGraveyard();
-                    StaticClass.Ennemy.DoDamage(StaticClass.AttackValue);
+                    Value -= Time.deltaTime;
+                    text.text = Value.ToString("0");
                 }
                 else
-                { 
-                    StaticClass.Attacker.CardInstanceToGraveyard();
-                    StaticClass.Player.DoDamage(StaticClass.AttackValue);
+                {
+                    if (Value <= 0.1)
+                    {
+                        Fight.IsFight = false;
+                    }
+                    //else if (Player1.IsAlive())
+                    //{
+                    //    StaticClass.Blocker.CardInstanceToGraveyard();
+                    //    StaticClass.Ennemy.DoDamage(StaticClass.AttackValue);
+                    //}
+                    //else
+                    //{ 
+                    //    StaticClass.Attacker.CardInstanceToGraveyard();
+                    //    StaticClass.Player.DoDamage(StaticClass.AttackValue);
+                    //}
+                    Fight.IsFight = false;
+                    //SceneManager.SetActiveScene(SceneManager.GetSceneByName("Test"));
+                    //SceneManager.UnloadScene("MainScene2");
                 }
-                Fight.IsFight = false;
-                SceneManager.SetActiveScene(SceneManager.GetSceneByName("Test"));
-                SceneManager.UnloadScene("MainScene2");
-            }
+            //}
         }
     }
 }
