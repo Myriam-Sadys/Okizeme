ebc1f322f9ea7d882235992ce7839831f079237c
Priscillia <priscillia.ponnou@epitech.eu>
2019-11-11 18:19:19 +0100

    Debug quit Button, add Rules + Player vs AI Button, Screen Connection and add Game Instance Manager to Save Player Data on different session (auto login when lose game and exit)

---
+++ OkizemeFighting/Assets/Scripts/AccountDisconnect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AccountDisconnect : MonoBehaviour
+{
+    [SerializeField]
+    string SceneToLoad = "Menu";
+    [SerializeField]
+    private GameObject AccountConnection;
+    [SerializeField]
+    private GameObject LoadingScreen;
+    [SerializeField]
+    private GameObject Canvas;
+    [SerializeField]
+    private GameObject MenuCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Disconnect()
+    {
+        GameInstanceManager.m_gim.UnsetToken();
+        Application.LoadLevel(SceneToLoad);
+    }
+}
---
+++ OkizemeFighting/Assets/Scripts/Managers/GameInstanceManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInstanceManager : MonoBehaviour
+{
+    public static GameInstanceManager m_gim;
+
+
+    public string m_userName { get; set; }
+    public string m_password { get; set; }
+    public string m_email { get; set; }
+    public string m_token { get; set; }
+
+    private void Awake()
+    {
+        if (m_gim == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            m_gim = this;
+        }
+        else if (m_gim != this)
+            Destroy(gameObject);
+
+        m_userName = PlayerPrefs.GetString("User", "");
+        m_email = PlayerPrefs.GetString("Email", "");
+        m_password = PlayerPrefs.GetString("Password", "");
+        m_token = PlayerPrefs.GetString("Token", "");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UnsetToken()
+    {
+        m_gim.m_token = "";
+        PlayerPrefs.SetString("Token", "");
+    }
+}
